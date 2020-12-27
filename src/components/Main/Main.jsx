3d35c04f875294445c42547b16f64c4e1adbecd7
ShotAlex <shot.aleks@gmail.com>
2020-12-27 04:25:14 +0300

    feat: add time left static

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,14 +3,18 @@ import './Main.scss';
 
 const Main = () => {
     const [currentDate, setCurrentDate] = useState('')
+    const [timeLeft, setTimeLeft] = useState({
+        days: 12,
+        hours: 4,
+        minutes: 32,
+        seconds: 52
+    })
 
     useEffect(() => {
         setInterval(() => {
             getCurrentDate()
         }, 1000)
     }, [])
-
-
 
     const getCurrentDate = () => {
         const date = new Date()
@@ -24,21 +28,38 @@ const Main = () => {
         setCurrentDate(date)
     }
 
-
     return (
         <div className="main">
             <h1 className="main__title">Countdown Timer</h1>
+
             <div className="main-name-event">
                 Input U event and click start
             </div>
-            <div className="main-date">
-                Output date
+
+            <div className="main-countdown">
+                <div className="main-countdown__item">
+                    {timeLeft.days}
+                    <span>days</span>
+                </div>
+                <div className="main-countdown__item">
+                    {timeLeft.hours}
+                    <span>hours</span>
+                </div>
+                <div className="main-countdown__item">
+                    {timeLeft.minutes}
+                    <span>min</span>
+                </div>
+                <div className="main-countdown__item">
+                    {timeLeft.seconds}
+                    <span>sec</span>
+                </div>
             </div>
+
             <div className="main-current-date">
                 Now: {currentDate}
             </div>
         </div>
-    );
+    )
 }
 
 export default Main;
