4068f9d8220948634dd1ae189e4628dfa5e09f71
ShotAlex <shot.aleks@gmail.com>
2020-12-27 05:32:36 +0300

    feat: add difference date function

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,32 +1,47 @@
 import React, {useState,useEffect} from "react";
 import './Main.scss';
 
+const DAY_MS = 8.64e7;
+const HOUR_MS = 3.6e6;
+const MIN_MS = 6e4;
+const SEC_MS = 1e3;
+
 const Main = () => {
-    const [currentDate, setCurrentDate] = useState('')
     const [timeLeft, setTimeLeft] = useState({
-        days: 12,
-        hours: 4,
-        minutes: 32,
-        seconds: 52
+        days: undefined,
+        hours: undefined,
+        minutes: undefined,
+        seconds: undefined
     })
 
     useEffect(() => {
         setInterval(() => {
-            getCurrentDate()
+        getDateDifference()
         }, 1000)
     }, [])
 
-    const getCurrentDate = () => {
-        const date = new Date()
-            .toLocaleString("ru", {
-                day: 'numeric',
-                hour: 'numeric',
-                minute: 'numeric',
-                second: 'numeric'
-            })
-
-        setCurrentDate(date)
+    const getDateDifference = () => {
+        const dateNow = new Date();
+        const inputDate = new Date(2020,11, 31, 23, 59, 59)
+        const currencyMilliseconds = inputDate.getTime() - dateNow.getTime()
+        convertMillisecondsToDayHoursMinSec(currencyMilliseconds)
     }
+
+    const convertMillisecondsToDayHoursMinSec = (milliseconds) => {
+        const days = Math.floor(milliseconds / DAY_MS);
+        const hours = Math.floor(milliseconds % DAY_MS / HOUR_MS);
+        const minutes = Math.floor(milliseconds % HOUR_MS / MIN_MS);
+        const seconds = Math.floor(milliseconds % MIN_MS / SEC_MS);
+
+        setTimeLeft({
+            days,
+            hours,
+            minutes,
+            seconds
+        })
+    }
+
+
 
     return (
         <div className="main">
@@ -53,10 +68,6 @@ const Main = () => {
                     {timeLeft.seconds}
                     <span>sec</span>
                 </div>
-            </div>
-
-            <div className="main-current-date">
-                Now: {currentDate}
             </div>
         </div>
     )
