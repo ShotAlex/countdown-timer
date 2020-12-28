312a63c9c9a3c35721932c04f276c755cbe7af02
ShotAlex <shot.aleks@gmail.com>
2020-12-28 22:39:00 +0300

    refactor: delete comment

--- src/components/ClearTimer/ClearTimer.jsx
+++ src/components/ClearTimer/ClearTimer.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import './ClearTimer.scss'
 import stopImg from './../../assets/images/stop-button.svg'
+import './ClearTimer.scss'
 
 const ClearTimer = ({clearTimer}) => {
-
-
     return (
         <button
             className="clear-timer"
             onClick={clearTimer}
         >
-            {/*CLEAR*/}
             <img src={stopImg} alt="Stop timer"/>
         </button>
     );
